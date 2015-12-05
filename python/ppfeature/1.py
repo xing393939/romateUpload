85716f05892719ca8b87dee911fc0239b1ecfc4d
chenxing <xing393939@qq.com>
2015-12-05 13:07:44 +0800

    js-typed-array-sha1

--- python/ppfeature/1.py
+++ python/ppfeature/1.py
@@ -5,19 +5,26 @@ path = "C:\\1.html"
 
 h = hashlib.sha1()
 size = os.path.getsize(path)
-print size
 with open(path, 'rb') as stream:
     if size < 0xFFFF:
         h.update(stream.read())
     else:
         h.update(stream.read(0x3000))
+        print 0, h.hexdigest()
+
         stream.seek(size/5)
         h.update(stream.read(0x3000))
+        print 1*size/5, h.hexdigest()
+
         stream.seek(2*size/5)
         h.update(stream.read(0x3000))
+        print 2*size/5, h.hexdigest()
+
         stream.seek(3*size/5)
         h.update(stream.read(0x3000))
+        print 3*size/5, h.hexdigest()
+
         stream.seek(size-0x3000)
         h.update(stream.read(0x3000))
-print h.hexdigest()
+print size-0x3000, h.hexdigest()
 
