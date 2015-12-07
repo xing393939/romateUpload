78a380ef1a8aa540e185caa3a494d96a6326cca1
chenxing <xing393939@qq.com>
2015-12-07 22:23:25 +0800

    1

--- php/1.php
+++ php/1.php
@@ -1,6 +1,16 @@
 <?php
-/*echo 0x5000;
-die();*/
-file_put_contents('1.wmv', '1', FILE_APPEND);
-//echo md5_file('1.wmv');
+$file_size = filesize('1.wmv');
+$fp = fopen('1.wmv', 'r');
+$input = fread($fp, 0x40000);
+fclose($fp);
+
+$target = dirname(__DIR__) . '/uploads/tmp.rar';
+$num = file_put_contents($target, $input);
+
+$sha1 = sha1_file($target);
+for ($i = 1; $i <= ceil($file_size / 0x40000); $i ++) {
+    echo $sha1, '<br>';
+    $sha1 = sha1($sha1);
+}
+echo $sha1, '<br>';
 ?>
